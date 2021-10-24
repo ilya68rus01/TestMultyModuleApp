336215d3d5c9faf177ebfac2a5168bdde23bb1f9
ilya <ilya68rus01@gmail.com>
2021-10-24 16:43:43 +0300

    Remove unused imports

--- feature/firstfeature/src/main/java/ilya/khrushchev/firstfeature/FirstFeatureDependencies.kt
+++ feature/firstfeature/src/main/java/ilya/khrushchev/firstfeature/FirstFeatureDependencies.kt
@@ -1,10 +1,7 @@
 package ilya.khrushchev.firstfeature
 
-import com.example.moduleinjector.BaseDependencyHolder
 import com.example.moduleinjector.BaseFeatureDependencies
-import ilya.khrushchev.core.providers.ContextProvider
 import ilya.khrushchev.core.providers.FirstFeatureDeps
-import ilya.khrushchev.core.providers.NavigationCommand
 
 interface FirstFeatureDependencies : BaseFeatureDependencies {
     val contextProvider: FirstFeatureDeps
